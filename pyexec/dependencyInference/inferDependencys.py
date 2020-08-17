a5b84d87fb52b9f1f8d7c1234d8e19805e687bcc
Michael Pusl <pusl03@ads.uni-passau.de>
2020-08-17 11:34:21 +0200

    Added logging

--- pyexec/dependencyInference/inferDependencys.py
+++ pyexec/dependencyInference/inferDependencys.py
@@ -1,8 +1,10 @@
 import subprocess
+from logging import Logger
 from os import path
 from typing import List, Optional
 
 from pyexec.util.list import flatten, remove_duplicates
+from pyexec.util.logging import get_logger
 from pyexec.util.shell import run_command
 
 
@@ -20,8 +22,9 @@ class InferDockerfile:
         pass
 
     __projectPath: str
+    __logger: Logger
 
-    def __init__(self, projectPath: str) -> None:
+    def __init__(self, projectPath: str, logfile: Optional[str] = None) -> None:
         if not path.exists(projectPath):
             raise InferDockerfile.DirectoryNotFoundExcpetion(
                 "There is no file or directory named " + projectPath
@@ -32,28 +35,28 @@ class InferDockerfile:
             )
         else:
             self.__projectPath = projectPath
+            self.__logger = get_logger(__name__, logfile)
 
     def inferDockerfile(self, timeout: Optional[int] = None) -> str:
-        files: List[str] = InferDockerfile.__find_python_files(self.__projectPath)
+        self.__logger.info("Start inferring dependencies")
+        files: List[str] = self.__find_python_files()
+        self.__logger.debug("Files found: ")
+        self.__logger.debug(*files)
         dockerfiles: List[str] = []
 
         for f in files:
-            dockerfiles.append(
-                InferDockerfile.__execute_v2(self.__projectPath, f, timeout)
-            )
-        return InferDockerfile.__mergeDockerfiles(dockerfiles)
+            self.__logger.debug("Inferring file: " + f)
+            dockerfiles.append(self.__execute_v2(f, timeout))
+            self.__logger.debug("Inferring for file " + f + "successful")
+        return self.__mergeDockerfiles(dockerfiles)
 
-    @staticmethod
-    def __find_python_files(projectPath: str) -> List[str]:
-        command: str = "find " + projectPath + " -type f -name '*.py' -not -name 'test*' -not -name '__init__.py'"
+    def __find_python_files(self) -> List[str]:
+        command: str = "find " + self.__projectPath + " -type f -name '*.py' -not -name 'test*' -not -name '__init__.py'"
         out, _ = run_command(command)
         return out.splitlines()
 
-    @staticmethod
-    def __execute_v2(
-        projectPath: str, filePath: str, timeout: Optional[int] = None
-    ) -> str:
-        command: str = 'v2 run --projectdir "' + projectPath + '" --environment "PYTHONPATH=$(find' + projectPath + " -not -path '*/\\.*' -not path '*__pycache__*' -type d -printf \":%p\" | sed \"s|" + projectPath + '|/mnt/projectdir/|g")" ' + filePath
+    def __execute_v2(self, filePath: str, timeout: Optional[int] = None) -> str:
+        command: str = 'v2 run --projectdir "' + self.__projectPath + '" --environment "PYTHONPATH=$(find' + self.__projectPath + " -not -path '*/\\.*' -not path '*__pycache__*' -type d -printf \":%p\" | sed \"s|" + self.__projectPath + '|/mnt/projectdir/|g")" ' + filePath
 
         try:
             out, _ = run_command(command, timeout)
@@ -70,10 +73,11 @@ class InferDockerfile:
         else:
             return out
 
-    @staticmethod
-    def __mergeDockerfiles(dockerfiles: List[str]) -> str:
+    def __mergeDockerfiles(self, dockerfiles: List[str]) -> str:
         lines: List[List[str]] = [f.splitlines() for f in dockerfiles]
         for f in lines:
             f.pop()
             f.pop(1)
-        return "\n".join(remove_duplicates(flatten(lines)))
+        file = "\n".join(remove_duplicates(flatten(lines)))
+        self.__logger.debug("The found dockerfile is: " + file)
+        return file
---
+++ pyexec/util/logging.py
@@ -0,0 +1,24 @@
+import logging
+from typing import Optional
+
+
+def get_logger(name: str, filename: Optional[str] = None) -> logging.Logger:
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    if filename:
+        log_file = logging.FileHandler(filename)
+        log_file.setFormatter(
+            logging.Formatter(
+                "%(asctime)s [%(levelname)s](%(name)s:%(funcName)s:%(lineno)d): "
+                "%(message)s"
+            )
+        )
+        log_file.setLevel(logging.DEBUG)
+        logger.addHandler(log_file)
+
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    console.setFormatter(logging.Formatter("[%(levelname)s](%(name)s): %(message)s"))
+    logger.addHandler(console)
+    return logger
