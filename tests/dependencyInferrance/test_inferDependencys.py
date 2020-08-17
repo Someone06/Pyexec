63ba2eea0ecf7013e25beca818ae9e519a906d88
Michael Pusl <pusl03@ads.uni-passau.de>
2020-08-17 09:22:37 +0200

    Added v2 test and slow marker

--- tests/dependencyInferrance/test_inferDependencys.py
+++ tests/dependencyInferrance/test_inferDependencys.py
@@ -1,8 +1,19 @@
+import pytest
+
 from pyexec.dependencyInference.inferDependencys import InferDockerfile
 
 
+@pytest.mark.slow
 def test_bayes():
     id: InferDockerfile = InferDockerfile("/home/michael/v2/examples/bayes/")
     result: str = id.inferDockerfile()
     print(result)
     assert "Theano" in result and "Lasagne" in result
+
+
+@pytest.mark.slow
+def test_test_runner():
+    id: InferDockerfile = InferDockerfile("/home/michael/test-runner")
+    result = id.inferDockerfile()
+    print(result)
+    assert False
